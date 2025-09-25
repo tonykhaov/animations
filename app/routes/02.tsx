b29280d6738d16f6bbed49f90578c8b86f3e8942
Tony Khaov <tony.khaov@gmail.com>
2025-09-25 08:54:25 +0200

    02: improve bottom sheet

--- app/routes/02.tsx
+++ app/routes/02.tsx
@@ -1,4 +1,4 @@
-import { animated, useSpring } from '@react-spring/web'
+import { animated, useSpring, useSpringValue } from '@react-spring/web'
 import { useDrag } from '@use-gesture/react'
 import * as React from 'react'
 import { createPortal } from 'react-dom'
@@ -23,11 +23,7 @@ export default function Two() {
         </button>
       )}
       {typeof window !== 'undefined' &&
-        createPortal(
-          <BottomSheet isOpen={isOpen} close={() => setIsOpen(false)} />,
-
-          document.body,
-        )}
+        createPortal(<BottomSheet isOpen={isOpen} close={() => setIsOpen(false)} />, document.body)}
     </div>
   )
 }
@@ -39,32 +35,56 @@ function BottomSheet({ isOpen, close }: { isOpen: boolean; close: () => void }) 
     x: 0,
     y: 0,
   }))
-  const bind = useDrag(({ down, movement: [mx, my], currentTarget }) => {
-    api.start({
-      y: down ? Math.max(my, 0) : 0,
-    })
+  const bind = useDrag(({ down, movement: [mx, my] }) => {
+    const isReleased = !down
 
     const threshold = elementRef.current!.offsetHeight / 3
-    const isReleased = !down
-    if (isReleased && my > threshold) {
-      close()
+
+    const shouldClose = isReleased && my > threshold
+    if (shouldClose) {
+      api.start({
+        y: window.innerHeight,
+        onResolve: close,
+        config: {
+          duration: 200,
+        },
+      })
+    } else {
+      api.start({
+        y: down ? Math.max(my, -100) : 0,
+      })
     }
   })
 
   return (
     <animated.div
+      data-open={isOpen}
+      className="fixed bg-red-500/10 inset-0 translate-y-full data-[open=true]:translate-y-0"
       ref={elementRef}
-      className="fixed max-w-lg mx-auto bottom-0 left-0 right-0 h-96 touch-none"
-      {...bind()}
       style={styles}
     >
       <div
-        className="h-full bg-green-600 origin-bottom rounded-t-3xl data-[open=false]:translate-y-full data-[open=true]:translate-y-0 transition-transform"
         data-open={isOpen}
+        className="h-full mx-auto w-xl bg-green-600 origin-bottom rounded-t-3xl translate-y-full transition-transform data-[open=true]:translate-y-1/3"
+        {...bind()}
       >
-        <div className="h-6" />
-        <header className="h-1 bg-white rounded-lg w-1/4 mx-auto" />
+        <div>
+          <div className="h-5" />
+          <header className="h-1 bg-white rounded-lg w-1/4 mx-auto" />
+        </div>
       </div>
     </animated.div>
   )
+  // return (
+  // <div
+  //   ref={elementRef}
+  //   className="z-1 fixed max-w-lg mx-auto bottom-0 left-0 right-0 touch-none"
+  //   {...bind()}
+  //   // style={styles}
+  // >
+  //   <div data-open={isOpen} className="h-screen data-[open=false]:translate-y-full data-[open=true]:translate-y-1/3">
+
+  //   </div>
+  // </div>
+  // )
 }
